990738a2db0766fc58d63419120224a5be3846c9
Reidner_Santos <reidner.santos@estudante.ifb.edu.br>
2023-04-20 23:59:21 +0000

    print arrumado

--- lib/view/lista_carro.dart
+++ lib/view/lista_carro.dart
@@ -15,7 +15,8 @@ class _HomeState extends State<Home> {
     setState(() {
       carros.insert(0, _carroController.text);
       anoController.insert(0, _anoController.text);
-      corControoller.insert(
+
+      corController.insert(
           0,
           int.tryParse(_corController.text) != null
               ? int.parse(_corController.text)
@@ -28,7 +29,7 @@ class _HomeState extends State<Home> {
   }
 
   final List<String> carros = <String>["FJK-39M", "BFK-39U", "SFK-20U"];
-  final List<int> corControoller = [40, 30, 20];
+  final List<int> corController = [40, 30, 20];
 
   List<String> anoController = ['2023', '2022', '2019'];
 
@@ -103,14 +104,14 @@ class _HomeState extends State<Home> {
                 return Container(
                   height: 50,
                   margin: EdgeInsets.all(2.0),
-                  color: corControoller[index] >= 2019
+                  color: corController[index] >= 2019
                       ? Colors.blue[400]
-                      : corControoller[index] >= 2020
+                      : corController[index] >= 2020
                           ? Colors.blue[100]
                           : Colors.grey,
                   child: Center(
                     child: Text(
-                      "placas ${carros[index]}  cor : ${corControoller[index]} ano ${anoController[index]}",
+                      "placas ${carros[index]}  cor : ${corController[index]} ano ${anoController[index]}",
                       style: TextStyle(fontSize: 18),
                     ),
                   ),
